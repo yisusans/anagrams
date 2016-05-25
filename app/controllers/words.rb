5782550c34fd53845c2eb753dba21b92c5e8c937
Susan Yi <yi.susans@gmail.com>
2016-05-25 00:37:15 -0400

    refactoring

--- app/controllers/words.rb
+++ app/controllers/words.rb
@@ -1,11 +1,11 @@
 get '/words/:word' do
   @word = params[:word]
-  word_in_table = Word.all.find_by(word: @word)
+  word_in_db = Word.all.find_by(word: @word)
 
-  if word_in_table == nil
+  if word_in_db == nil
     erb :"words/error"
   else
-    @anagram_array = word_in_table.anagrams
+    @anagram_array = word_in_db.anagrams
   # Look in app/views/words/index.erb
     erb :"words/index"
   end
--- app/models/word.rb
+++ app/models/word.rb
@@ -4,20 +4,15 @@ class Word < ActiveRecord::Base
   def anagrams
     split_word = self.word.split('').sort
     equal_length_words.select do |word|
-      word.split('').sort == split_word
-    end - [self.word]
+      word.word.split('').sort == split_word && word.word != self.word
+    end
   end
 
   private
 
-  def words_only_array
-    list = Word.all
-    list.map { |word| word.word }
-  end
-
   def equal_length_words
-    words_only_array.select do |word|
-      word.length == self.word.length
+    Word.all.select do |word|
+      word.word.length == self.word.length
     end
   end
 
